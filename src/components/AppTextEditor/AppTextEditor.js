4c8fb86dfa4a0108dbc1d6c14ac7cf9eddcd666c
avijit125 <moupiyachanda1994@gmail.com>
2023-06-30 01:46:03 -0700

    fixed apptext editor

--- src/components/AppTextEditor/AppTextEditor.js
+++ src/components/AppTextEditor/AppTextEditor.js
@@ -1,32 +1,45 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
+import { EditorState, ContentState } from "draft-js";
 import { convertToHTML, convertFromHTML } from "draft-convert";
 import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./AppTextEditor.scss";
-const AppTextEditor = ({ value, setFieldValue }) => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [isFirstRender, setIsFirstRender] = useState(true);
-  useEffect(() => {
-    let html = convertToHTML(editorState.getCurrentContent());
-    setFieldValue(html);
-  }, [editorState, setFieldValue]);
-  useEffect(() => {
 
-    if (isFirstRender && value) {
-      const editState = EditorState.createWithContent(convertFromHTML(value));
-      setEditorState(editState);
-      setIsFirstRender(false);
+const AppTextEditor = ({ value, setFieldValue }) => {
+  const [editorState, setEditorState] = useState(null);
+
+  useEffect(() => {
+    if (value && !editorState) {
+      const contentState = convertFromHTML(value);
+      const initialEditorState = EditorState.createWithContent(contentState);
+      setEditorState(initialEditorState);
     }
-  }, [value, isFirstRender]);
+  }, [value, editorState]);
+
+  useEffect(() => {
+    if (editorState) {
+      const html = convertToHTML(editorState.getCurrentContent());
+      setFieldValue(html);
+    }
+  }, [editorState, setFieldValue]);
+
+  const handleEditorStateChange = (state) => {
+    setEditorState(state);
+  };
+
+  if (!editorState) {
+    return <div>Loading editor...</div>;
+  }
+
   return (
     <Editor
       editorState={editorState}
       wrapperClassName="wrapper-class"
       editorClassName="editor-class"
       toolbarClassName="toolbar-class"
-      onEditorStateChange={setEditorState}
+      onEditorStateChange={handleEditorStateChange}
     />
   );
 };
+
 export default AppTextEditor;
