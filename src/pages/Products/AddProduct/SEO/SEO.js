b2742dfc83b19725ae338b511967a60e48f97cd4
avijit125 <moupiyachanda1994@gmail.com>
2023-07-14 06:45:04 -0700

    minor changes done

--- src/pages/Products/AddProduct/SEO/SEO.js
+++ src/pages/Products/AddProduct/SEO/SEO.js
@@ -17,17 +17,21 @@ import {
 import { useFormik } from "formik";
 import { useCreateSeoMutation } from "../../../../features/seo/seoApiSlice";
 import { useDispatch } from "react-redux";
-import { showError, showSuccess } from "../../../../features/snackbar/snackbarAction";
-const SEO = ({ seoName, seoValue, handleSeoChange,refrenceId,toggleState }) => {
+import {
+  showError,
+  showSuccess,
+} from "../../../../features/snackbar/snackbarAction";
+import _ from "lodash";
+const SEO = ({ seoName, seoValue, handleSeoChange, refrenceId }) => {
   const [multipleTags, setMultipleTags] = useState([]);
   const [isFirstTimeRender, setIsFirstTimeRender] = useState(true);
-  const [retainValue,setReatinValue] = useState({})
-  const dispatch = useDispatch()
+  const [retainValue, setReatinValue] = useState(seoValue ? seoValue : {});
+  const dispatch = useDispatch();
   // ? CHECKBOX STARTS HERE
 
-  const [checkedSwitch, setCheckedSwitch] = React.useState(toggleState);
   const [viewAll, setViewAll] = useState(false);
   let seoItems = {};
+
 
   const [
     createSeo,
@@ -57,46 +61,27 @@ const SEO = ({ seoName, seoValue, handleSeoChange,refrenceId,toggleState }) => {
   // ? CHECKBOX ENDS HERE
   const seoFormik = useFormik({
     initialValues: {
-      title: "",
-      slug: "https://example.com/",
+      title: seoValue.title || seoName,
+      slug: "https://example.com/"+seoName,
+      description: seoValue.description ? seoValue.description : ""
     },
     enableReinitialize: true,
   });
   useEffect(() => {
     if (!isEmpty(seoValue) && isFirstTimeRender === true && seoName !== "") {
-      seoFormik.setFieldValue(
-        "title",
-        seoValue && seoValue.title ? seoValue.title : seoName
-      );
-      seoFormik.setFieldValue(
-        "description",
-        seoValue && seoValue.description ? seoValue.description : ""
-      );
-      seoFormik.setFieldValue(
-        "slug",
-        seoValue && seoValue.slug ? seoValue.slug : generateUrlName(seoName)
-      );
       setMultipleTags(
         seoValue && seoValue.metaKeywords ? seoValue.metaKeywords : []
       );
       setIsFirstTimeRender(false);
-      setCheckedSwitch(true);
-      setReatinValue(seoValue)
     }
 
     if (isEmpty(seoValue) && isFirstTimeRender === true && seoName !== "") {
-      seoFormik.setFieldValue("title", seoName);
-      seoFormik.setFieldValue("description", "");
-      seoFormik.setFieldValue("slug", generateUrlName(seoName));
       setMultipleTags([]);
       setIsFirstTimeRender(false);
-      setCheckedSwitch(false);
-      setReatinValue(seoValue)
     }
   }, [seoValue, seoName, isFirstTimeRender]);
 
   function handleSubmit() {
-   
     seoItems.title = seoFormik.values?.title;
     if (
       seoFormik.initialValues.slug !== seoFormik.values.slug ||
@@ -113,24 +98,22 @@ const SEO = ({ seoName, seoValue, handleSeoChange,refrenceId,toggleState }) => {
     handleSeoChange(seoItems);
   }
 
-  useEffect(()=>{
-    if(checkedSwitch === true){
-      if (multipleTags.length > 0) {
-        seoItems.metaKeywords = multipleTags;
-      }
-      if (seoFormik.values?.description) {
-        seoItems.description = seoFormik.values?.description;
-        seoItems.title = seoFormik.values?.title;
-        seoItems.slug = seoFormik.values.slug
-      }
+  useEffect(() => {
+    if (multipleTags.length > 0) {
+      seoItems.metaKeywords = multipleTags;
+    }
+    if ( !_.isEqual(seoFormik.initialValues.description, seoFormik.values.description)) {
+      seoItems.description = seoFormik.values?.description;
+      seoItems.title = seoFormik.values?.title;
+      seoItems.slug = seoFormik.values.slug;
       handleSeoChange(seoItems);
     }
-  },[seoFormik.values])
+  }, [seoFormik.values]);
 
-  function handleSeoCreate(){
+  function handleSeoCreate() {
     let seoItems = {};
     seoItems.title = seoFormik.values?.title;
-    seoItems.refrenceId =refrenceId
+    seoItems.refrenceId = refrenceId;
     if (
       seoFormik.initialValues.slug !== seoFormik.values.slug ||
       seoValue?.slug !== seoFormik.values.slug
@@ -143,20 +126,15 @@ const SEO = ({ seoName, seoValue, handleSeoChange,refrenceId,toggleState }) => {
     if (multipleTags.length > 0) {
       seoItems.metaKeywords = multipleTags;
     }
-    createSeo(seoItems).unwrap().then(()=>{
-      handleSeoChange({})
-     dispatch(showSuccess({message:"Seo created for this category"}))
-    }).catch((err)=> dispatch(showError({message: err?.data?.message})))
+    createSeo(seoItems)
+      .unwrap()
+      .then(() => {
+        handleSeoChange({});
+        dispatch(showSuccess({ message: "Seo created for this category" }));
+      })
+      .catch((err) => dispatch(showError({ message: err?.data?.message })));
   }
-  // ? SWITCH STARTS HERE
-  const handleSwitchChange = (event) => {
-    setCheckedSwitch(event.target.checked);
-    if (event.target.checked === false) {
-      handleSeoChange(retainValue);
-      return;
-    }
-    handleSubmit();
-  };
+
   // ? SWITCH ENDS HERE
   const handleAddMultiple = (event) => {
     if (event.key === "Enter") {
@@ -194,157 +172,149 @@ const SEO = ({ seoName, seoValue, handleSeoChange,refrenceId,toggleState }) => {
             />
           </Tooltip>
         </div>
-        <AntSwitch
-          inputProps={{ "aria-label": "ant design" }}
-          checked={checkedSwitch}
-          onChange={handleSwitchChange}
-        />
       </div>
-      {checkedSwitch && (
-        <React.Fragment>
-          <div className="d-flex align-items-center mt-3 col-12 px-0">
-            <FormControlLabel
-              control={
-                <Checkbox
-                  // checked={checked}
-                  // onChange={handleCheckboxChange}
-                  inputProps={{ "aria-label": "controlled" }}
-                  size="small"
-                  style={{
-                    color: "#5C6D8E",
-                    marginRight: 0,
-                  }}
-                />
-              }
-              label="Auto Generate SEO "
-              sx={{
-                "& .MuiTypography-root": {
-                  fontSize: 13,
-                  color: "#C8D8FF",
-                },
+
+      <div className="d-flex align-items-center mt-3 col-12 px-0">
+        <FormControlLabel
+          control={
+            <Checkbox
+              // checked={checked}
+              // onChange={handleCheckboxChange}
+              inputProps={{ "aria-label": "controlled" }}
+              size="small"
+              style={{
+                color: "#5C6D8E",
+                marginRight: 0,
               }}
             />
-            <p>
-              <i className="text-grey-6">Powered by Kepler</i>
-            </p>
-          </div>
-          <div className="col-12 px-0 bg-black-13 border-grey-5 mt-3 rounded-8">
-            <div className="d-flex flex-column p-3">
-              <div className="d-flex justify-content-between">
-                <p className="text-lightBlue">Metadata Preview</p>
-                {viewAll == false ? (
-                  <small
-                    className="text-lightBlue text-blue-2 c-pointer"
-                    onClick={() => {
-                      handleSubmit();
-                      setViewAll((prevState) => !prevState);
-                    }}
-                  >
-                    SEO Edit
-                  </small>
-                ) : (
-                  <small
-                    className="text-lightBlue text-blue-2 c-pointer"
-                    onClick={() => {
-                      handleSeoCreate();
-                      setViewAll((prevState) => !prevState);
-                    }}
-                  >
-                    Save
-                  </small>
-                )}
-              </div>
-              <small className="text-lightBlue mt-3 mb-2 fw-500">
-                {seoFormik.values.title}
-              </small>
-              <small className="text-blue-2">{seoFormik.values.slug}</small>
-              <small className="mt-2 text-grey-6">
-                {seoFormik.values.description}
-              </small>
-            </div>
-          </div>
-          {viewAll && (
-            <React.Fragment>
-              <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
-                Page Title
-              </small>
-              <FormControl className="col-12 px-0">
-                <OutlinedInput
-                  name="title"
-                  placeholder="Please enter page title"
-                  size="small"
-                  value={seoFormik.values.title}
-                  onChange={seoFormik.handleChange}
-                  onBlur={seoFormik.handleBlur}
-                />
-              </FormControl>
-              <small className="mt-1 text-grey-6 col-12 px-0">
-                20 of 50 characters Used
-              </small>
-              <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
-                Meta Description
-              </small>
-              <TextareaAutosize
-                name="description"
-                aria-label="meta description"
-                placeholder="Please enter meta description"
-                style={{
-                  background: "#15142A",
-                  color: "#C8D8FF",
-                  borderRadius: 5,
+          }
+          label="Auto Generate SEO "
+          sx={{
+            "& .MuiTypography-root": {
+              fontSize: 13,
+              color: "#C8D8FF",
+            },
+          }}
+        />
+        <p>
+          <i className="text-grey-6">Powered by Kepler</i>
+        </p>
+      </div>
+      <div className="col-12 px-0 bg-black-13 border-grey-5 mt-3 rounded-8">
+        <div className="d-flex flex-column p-3">
+          <div className="d-flex justify-content-between">
+            <p className="text-lightBlue">Metadata Preview</p>
+            {viewAll == false ? (
+              <small
+                className="text-lightBlue text-blue-2 c-pointer"
+                onClick={() => {
+                  handleSubmit();
+                  setViewAll((prevState) => !prevState);
                 }}
-                minRows={5}
-                value={seoFormik.values.description}
-                onChange={seoFormik.handleChange}
-                onBlur={seoFormik.handleBlur}
-                className="col-12"
-              />
-              <small className="mt-1 text-grey-6 col-12 px-0">
-                200 of 500 characters Used
+              >
+                SEO Edit
               </small>
-              <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
-                URL Handle
+            ) : (
+              <small
+                className="text-lightBlue text-blue-2 c-pointer"
+                onClick={() => {
+                  handleSeoCreate();
+                  setViewAll((prevState) => !prevState);
+                }}
+              >
+                Save
               </small>
-              <FormControl className="col-12 px-0">
-                <OutlinedInput
-                  name="slug"
-                  placeholder="Please enter Url"
-                  size="small"
-                  value={seoFormik.values.slug}
-                  onChange={seoFormik.handleChange}
-                  onBlur={seoFormik.handleBlur}
-                />
-              </FormControl>
-              <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
-                Meta Keywords
-              </small>
-              <FormControl className="col-12 px-0">
-                <OutlinedInput
-                  name="metaKeywords"
-                  value={seoFormik.values.metaKeywords}
-                  onChange={seoFormik.handleChange}
-                  onBlur={seoFormik.handleBlur}
-                  onKeyDown={handleAddMultiple}
-                  placeholder="Enter keywords"
-                  size="small"
-                />
-                <div className="d-flex">
-                  {multipleTags &&
-                    multipleTags.map((data) => {
-                      return (
-                        <Chip
-                          label={data}
-                          onDelete={() => handleDelete(data)}
-                          onClick={() => {}}
-                          size="small"
-                          className="mt-3 me-2"
-                        ></Chip>
-                      );
-                    })}
-                </div>
-              </FormControl>
-            </React.Fragment>
-          )}
+            )}
+          </div>
+          <small className="text-lightBlue mt-3 mb-2 fw-500">
+            {seoFormik.values.title}
+          </small>
+          <small className="text-blue-2">{seoFormik.values.slug}</small>
+          <small className="mt-2 text-grey-6">
+            {seoFormik.values.description}
+          </small>
+        </div>
+      </div>
+      {viewAll && (
+        <React.Fragment>
+          <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
+            Page Title
+          </small>
+          <FormControl className="col-12 px-0">
+            <OutlinedInput
+              name="title"
+              placeholder="Please enter page title"
+              size="small"
+              value={seoFormik.values.title}
+              onChange={seoFormik.handleChange}
+              onBlur={seoFormik.handleBlur}
+            />
+          </FormControl>
+          <small className="mt-1 text-grey-6 col-12 px-0">
+            20 of 50 characters Used
+          </small>
+          <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
+            Meta Description
+          </small>
+          <TextareaAutosize
+            name="description"
+            aria-label="meta description"
+            placeholder="Please enter meta description"
+            style={{
+              background: "#15142A",
+              color: "#C8D8FF",
+              borderRadius: 5,
+            }}
+            minRows={5}
+            value={seoFormik.values.description}
+            onChange={seoFormik.handleChange}
+            onBlur={seoFormik.handleBlur}
+            className="col-12"
+          />
+          <small className="mt-1 text-grey-6 col-12 px-0">
+            200 of 500 characters Used
+          </small>
+          <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
+            URL Handle
+          </small>
+          <FormControl className="col-12 px-0">
+            <OutlinedInput
+              name="slug"
+              placeholder="Please enter Url"
+              size="small"
+              value={seoFormik.values.slug}
+              onChange={seoFormik.handleChange}
+              onBlur={seoFormik.handleBlur}
+            />
+          </FormControl>
+          <small className="text-lightBlue mb-2 mt-3 col-12 px-0">
+            Meta Keywords
+          </small>
+          <FormControl className="col-12 px-0">
+            <OutlinedInput
+              name="metaKeywords"
+              value={seoFormik.values.metaKeywords}
+              onChange={seoFormik.handleChange}
+              onBlur={seoFormik.handleBlur}
+              onKeyDown={handleAddMultiple}
+              placeholder="Enter keywords"
+              size="small"
+            />
+            <div className="d-flex">
+              {multipleTags &&
+                multipleTags.map((data) => {
+                  return (
+                    <Chip
+                      label={data}
+                      onDelete={() => handleDelete(data)}
+                      onClick={() => {}}
+                      size="small"
+                      className="mt-3 me-2"
+                    ></Chip>
+                  );
+                })}
+            </div>
+          </FormControl>
         </React.Fragment>
       )}
     </div>
