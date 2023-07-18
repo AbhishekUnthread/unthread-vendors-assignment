83310f39945284bbb25ef3ecb4604e83afc6430e
ayush_bytive <ayushsinghal@bytive.in>
2023-07-19 03:06:01 +0530

    discount

--- src/pages/Offers/ProductStatusToggle.js
+++ src/pages/Offers/ProductStatusToggle.js
@@ -2,41 +2,36 @@ import React from "react";
 // ! MATERIAL IMPORTS
 import { ToggleButtonGroup, ToggleButton } from "@mui/material";
 
-const ProductStatusToggle = () => {
-  // ? TOGGLE BUTTONS STARTS HERE
-  const [productStatus, setPoductStatus] = React.useState("active");
-  const handleProductStatus = (event, newProductStatus) => {
-    setPoductStatus(newProductStatus);
-  };
-  // ? TOGGLE BUTTONS ENDS HERE
+const ProductStatusToggle = ({value,toggleData,handleStatus,error}) => {
+
 
   return (
     <ToggleButtonGroup
-      value={productStatus}
-      onChange={handleProductStatus}
+      value={value}
+      onChange={handleStatus}
       aria-label="text formatting"
       className="row d-flex px-2 productInfo-toggle"
       size="small"
       exclusive
     >
       <ToggleButton
-        value="active"
+        value={toggleData[0]}
         aria-label="active"
         style={{ width: "50%" }}
         className="productInfo-toggle__active"
       >
         <div className="d-flex">
-          <p className="text-grey-6">Active</p>
+          <p className="text-grey-6">{toggleData[0]}</p>
         </div>
       </ToggleButton>
       <ToggleButton
-        value="inactive"
+        value={toggleData[1]}
         aria-label="inactive"
         style={{ width: "50%" }}
         className="productInfo-toggle__draft"
       >
         <div className="d-flex">
-          <p className="text-grey-6">In-Active</p>
+          <p className="text-grey-6">{toggleData[1]}</p>
         </div>
       </ToggleButton>
     </ToggleButtonGroup>
