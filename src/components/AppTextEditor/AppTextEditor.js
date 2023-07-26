89feb43b6bc7c3ffa4632b8d043c3690e4596d53
Deepak Kumar <deepak.kumar@bytive.in>
2023-07-26 11:28:24 +0530

    change text editor

--- src/components/AppTextEditor/AppTextEditor.js
+++ src/components/AppTextEditor/AppTextEditor.js
@@ -1,39 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { EditorState, ContentState } from "draft-js";
-import { convertToHTML, convertFromHTML } from "draft-convert";
-import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "./AppTextEditor.scss";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-const AppTextEditor = ({ value, setFieldValue }) => {
-  const [editorState, setEditorState] = useState(null);
-  useEffect(() => {
-    if (value && !editorState) {
-      const contentState = convertFromHTML(value);
-      const initialEditorState = EditorState.createWithContent(contentState);
-      setEditorState(initialEditorState);
-    }
-  }, [value, editorState]);
-  useEffect(() => {
-    if (editorState) {
-      const html = convertToHTML(editorState.getCurrentContent());
-      setFieldValue(html);
-    }
-  }, [editorState, setFieldValue]);
-  const handleEditorStateChange = (state) => {
-    setEditorState(state);
-  };
+import "./AppTextEditor.css";
 
-  // if (!editorState) {
-  //   return <div>Loading editor...</div>;
-  // }
+const AppTextEditor = ({ value, setFieldValue = (_) => {} }) => {
   return (
-    <Editor
-      editorState={editorState}
-      wrapperClassName="wrapper-class"
-      editorClassName="editor-class"
-      toolbarClassName="toolbar-class"
-      onEditorStateChange={handleEditorStateChange}
+    <CKEditor
+      editor={ClassicEditor}
+      data={value}
+      onChange={(_, editor) => {
+        const data = editor.getData();
+        setFieldValue(data);
+      }}
     />
   );
 };
